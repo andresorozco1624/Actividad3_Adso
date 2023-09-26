22de6051064578433ca51888c0a496d99eba5a8c
andresorozco1624 <108691375+andresorozco1624@users.noreply.github.com>
2023-09-25 20:32:37 -0500

    Se agrega un .js para la validacion de formulario por medio de la misma informacion que nos brinda bootstrap, sin embargo, deja los los campos en rojo después de enviar en formulario

--- asset/index.js
+++ asset/index.js
@@ -1,4 +1,4 @@
-function enviar(){
+function enviar() {
     // Creating variables bescause of reciving data from inputs on html
 
     var inputFullName = document.getElementById("fullName").value;
@@ -8,54 +8,70 @@ function enviar(){
     var inputSalaryIncome = document.getElementById("salaryIncome").value;
     var inputNumber = document.getElementById("number").value;
     var inputAvatar = document.getElementById("avatar").value;
-    
+
     //Creating objects because of organizing the each user's data 
-    var objUser ={ 
+    var objUser = {
         "fullName": inputFullName,
         "birthDate": inputBirthDate,
         "color": inputColor,
         "email": inputEmail,
         "salaryIncome": inputSalaryIncome,
         "number": inputNumber,
-        "avatar": inputAvatar 
+        "avatar": inputAvatar
     };
-  
+
 
     arrayUsers = loadData(objUser); //Download from Local Storage & edit data
     printTable(arrayUsers); // Print data on table
-    localStorage.setItem("userData",JSON.stringify(arrayUsers)); //Load data on LocaStorage
+    localStorage.setItem("userData", JSON.stringify(arrayUsers)); //Load data on LocaStorage
 
     resetForm.click(); //Reseting the data on form
+    
+    
+
+
 }
 
-function loadData(object){
+function loadData(object) {
     var arrayUsers = [];
     var userData = localStorage.getItem("userData");
-    if (userData === null){
-        localStorage.setItem("userData","[]");
+    if (userData === null) {
+        localStorage.setItem("userData", "[]");
     }
-    else{
+    else {
         arrayUsers = JSON.parse(userData);
     }
     arrayUsers.push(object);
     return arrayUsers;
 }
 
-function printTable (data){
-    var html ="";
-    for(var i=0; i < data.length; i++){
+function printTable(data) {
+    var html = "";
+    for (var i = 0; i < data.length; i++) {
         html += "<tr>";
-        html += "<th scope='row'>" + (i+1)+"</th>";
-        html += "<td>" + data[i].fullName +"</td>";
-        html += "<td>" + data[i].birthDate +"</td>";
-        html += "<td>" + data[i].color +"</td>";
-        html += "<td>" + data[i].email +"</td>";
-        html += "<td>" + data[i].salaryIncome +"</td>";
-        html += "<td>" + data[i].number +"</td>";
-        html += "<td>" + data[i].avatar +"</td>";
+        html += "<th scope='row'>" + (i + 1) + "</th>";
+        html += "<td>" + data[i].fullName + "</td>";
+        html += "<td>" + data[i].birthDate + "</td>";
+        html += "<td>" + data[i].color + "</td>";
+        html += "<td>" + data[i].email + "</td>";
+        html += "<td>" + data[i].salaryIncome + "</td>";
+        html += "<td>" + data[i].number + "</td>";
+        html += "<td>" + data[i].avatar + "</td>";
         html += "</tr>";
     }
-    bodyList.innerHTML= html;
-    
+    bodyList.innerHTML = html;
+
 }
+
+
+
+function deleteData1() {
+    localStorage.clear;
+}
+
+
+
+  
+
+
 
